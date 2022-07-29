7af175f6912dd2762abb7c2b3ee4a572541701f1
Lavius Motileng <103624603+Laviusntk@users.noreply.github.com>
2022-07-28 19:14:37 -0700

    Update SetupCondaAIEnvironment.sh

--- Configurations/Anaconda/SetupCondaAIEnvironment.sh
+++ Configurations/Anaconda/SetupCondaAIEnvironment.sh
@@ -1,4 +1,4 @@
 conda init zsh
-conda create --name datascience
-conda activate datascience
+conda create --name aiml
+conda activate aiml
 conda env update --file anaconda_ai_tools_environment.yaml
